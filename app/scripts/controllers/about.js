f6b69bacd295dc78f4ab627753b30056fdc9cd22
SupportDevGit <jacobroy87419@gmail.com>
2016-09-22 19:24:34 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gruntgitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gruntgitApp
+ */
+angular.module('gruntgitApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gruntgitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gruntgitApp
+ */
+angular.module('gruntgitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
